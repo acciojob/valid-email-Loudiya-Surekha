e5bd2ffdd5edde77bba331fb906f5327fd65eb0b
Loudiya Surekha <lsurekha2002@gmail.com>
2025-06-18 19:38:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function validEmail(str) {
   //your JS code here.
+	if(!str){
+		return false;  
+	}
+	const emailRegex= /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+	if(str.includes("..")){
+		return false;
+	}   
+	return emailRegex.test(str);
 }
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
+
+// str=lsurekha2002@gmail.com
